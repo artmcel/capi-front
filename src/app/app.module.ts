6489e75df0c06bcc6f50394cfefad187d48f0c44
arturo <artmcel@hotmail.com>
2023-10-12 17:23:43 -0600

    generate component tabla_usuarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TablaUsuariosComponent } from './tabla-usuarios/tabla-usuarios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TablaUsuariosComponent
   ],
   imports: [
     BrowserModule
