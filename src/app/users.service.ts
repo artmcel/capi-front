540aa44bba1d9b8b9ec5e6426f4d8a925483afa5
arturo <artmcel@hotmail.com>
2023-10-12 17:31:18 -0600

    add httpClient

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -1,9 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  constructor( private http:HttpClient) {
+
+  }
 }
