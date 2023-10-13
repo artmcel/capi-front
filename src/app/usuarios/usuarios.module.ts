58c1d932bd5ff05b762c83cb46983137c716f7e1
arturo <artmcel@hotmail.com>
2023-10-12 19:55:37 -0600

    add tabla component

--- src/app/usuarios/usuarios.module.ts
+++ src/app/usuarios/usuarios.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TablaUsuariosComponent } from '../tabla-usuarios/tabla-usuarios.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TablaUsuariosComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    TablaUsuariosComponent
   ]
 })
 export class UsuariosModule { }
