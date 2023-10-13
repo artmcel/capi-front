d9279f87fcc606c1e0e060f494a3037bcd6496e9
arturo <artmcel@hotmail.com>
2023-10-12 19:55:03 -0600

    import usuarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TablaUsuariosComponent } from './tabla-usuarios/tabla-usuarios.component';
 import { HttpClientModule } from '@angular/common/http';
+import { UsuariosModule } from './usuarios/usuarios.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TablaUsuariosComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    UsuariosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
